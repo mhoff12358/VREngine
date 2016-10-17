4606cdc1054b09fb6186bcd16ff4b72738ef4c83
mhoff12358 <mhoff12358@gmail.com>
2016-10-17 01:03:48 -0700

    Slight cleanup of the python interface file.

--- WandDemoPython/SceneSystemPython.cpp
+++ WandDemoPython/SceneSystemPython.cpp
@@ -122,28 +122,6 @@ BOOST_PYTHON_MODULE(scene_system_) {
 	class_<game_scene::commands::TimeTick, bases<game_scene::CommandArgs>, boost::noncopyable>("TimeTick", no_init)
 		.def_readonly("duration", &game_scene::commands::TimeTick::duration_);
 
-	class_<game_scene::commands::HeadsetListenerRegistration, bases<game_scene::CommandArgs>, boost::noncopyable>("ListenerRegistration", no_init)
-		.def_readonly("register_not_unregister", &game_scene::commands::HeadsetListenerRegistration::register_not_unregister_)
-		.def_readonly("actor_to_register", &game_scene::commands::HeadsetListenerRegistration::actor_to_register_)
-		.def_readonly("listener_id", &game_scene::commands::HeadsetListenerRegistration::listener_id_);
-
-	class_<game_scene::commands::ControllerMovement, bases<game_scene::CommandArgs>, boost::noncopyable>("ControllerMovement", no_init)
-		.def_readonly("movement_vector", &game_scene::commands::ControllerMovement::movement_vector_);
-
-	class_<game_scene::commands::ControllerInformation, bases<game_scene::CommandArgs>, boost::noncopyable>("ControllerInformation", no_init)
-		.def_readonly("controller_number", &game_scene::commands::ControllerInformation::controller_number_);
-
-	class_<game_scene::commands::TriggerStateChange, bases<game_scene::commands::ControllerInformation>, boost::noncopyable>("TriggerStateChange", no_init)
-		.def_readonly("is_pulled", &game_scene::commands::TriggerStateChange::is_pulled_);
-
-	class_<game_scene::commands::TouchpadMotion, bases<game_scene::commands::ControllerInformation>, boost::noncopyable>("TouchpadMotion", no_init)
-		.def_readonly("position", &game_scene::commands::TouchpadMotion::position_)
-		.def_readonly("delta", &game_scene::commands::TouchpadMotion::delta_);
-
-#include "io_interface.ipp"
-#include "graphics_resources.ipp"
-#include "pose.ipp"
-
 	void (PipelineCamera::*SetLocationFromLocation)(const Location&) = &PipelineCamera::SetLocation;
 	void (PipelineCamera::*SetOrientationFromQuaternion)(const Quaternion&) = &PipelineCamera::SetOrientation;
 	class_<PipelineCamera, boost::noncopyable>("PipelineCamera", no_init)
@@ -160,6 +138,10 @@ BOOST_PYTHON_MODULE(scene_system_) {
 		.def_readonly("xyuv", make_getter(&VertexType::xyuv, return_value_policy<reference_existing_object>()));
 
 #include "stl.ipp"
+#include "headset.ipp"
+#include "io_interface.ipp"
+#include "graphics_resources.ipp"
+#include "pose.ipp"
 #include "entity_specification.ipp"
 #include "graphics_object.ipp"
 }
