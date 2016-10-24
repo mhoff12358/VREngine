2b13eef9698ad7372ca8af40befcae1f80ab2bd9
mhoff12358 <mhoff12358@gmail.com>
2016-10-24 00:17:54 -0700

    Moves the model generation code out of the main scene system python definition file.

--- WandDemoPython/SceneSystemPython.cpp
+++ WandDemoPython/SceneSystemPython.cpp
@@ -30,6 +30,7 @@
 #include "IOInterfaceInterface.h"
 #include "GraphicsResourcesInterface.h"
 #include "PoseInterface.h"
+#include "ModelCreationInterface.h"
 
 #define BOOST_PTR_MAGIC(class_name) \
 namespace boost { \
@@ -47,18 +48,6 @@ BOOST_PTR_MAGIC(game_scene::actors::GraphicsResources)
 BOOST_PTR_MAGIC(game_scene::commands::IOListenerRegistration)
 BOOST_PTR_MAGIC(game_scene::commands::CreateNewGraphicsObject)
 BOOST_PTR_MAGIC(game_scene::commands::PlaceNewComponent)
-
-template<size_t N>
-void AddVerticesConstructor(class_<Vertices>& vertices);
-
-template<>
-void AddVerticesConstructor<0>(class_<Vertices>& vertices) {}
-
-template<size_t N>
-void AddVerticesConstructor(class_<Vertices>& vertices) {
-	vertices.def(init<VertexType, vector<array<float, N>>>());
-	AddVerticesConstructor<N - 1>(vertices);
-}
 
 BOOST_PYTHON_MODULE(scene_system_) {
 	class_<PyActor, boost::noncopyable>("Actor")
@@ -126,49 +115,6 @@ BOOST_PYTHON_MODULE(scene_system_) {
 		.def("SetPose", &PipelineCamera::SetPose)
 		.def("BuildMatrices", &PipelineCamera::BuildMatrices);
 
-	class_<VertexType>("VertexType", no_init)
-		.def_readonly("location", &VertexType::vertex_type_location)
-		.def_readonly("texture", &VertexType::vertex_type_texture)
-		.def_readonly("normal", &VertexType::vertex_type_normal)
-		.def_readonly("all", &VertexType::vertex_type_all)
-		.def_readonly("xyuv", &VertexType::xyuv);
-
-	auto vertices_registration = class_<Vertices>("Vertices", init<VertexType, vector<float>>());
-	AddVerticesConstructor<12>(vertices_registration);
-
-	enum_<D3D_PRIMITIVE_TOPOLOGY>("D3DTopology")
-		.value("POINTLIST", D3D_PRIMITIVE_TOPOLOGY_POINTLIST)
-		.value("LINELIST", D3D_PRIMITIVE_TOPOLOGY_LINELIST)
-		.value("LINESTRIP", D3D_PRIMITIVE_TOPOLOGY_LINESTRIP)
-		.value("TRIANGLELIST", D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST)
-		.value("TRIANGLESTRIP", D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP)
-		.value("LINELIST_ADJ", D3D_PRIMITIVE_TOPOLOGY_LINELIST_ADJ)
-		.value("LINESTRIP_ADJ", D3D_PRIMITIVE_TOPOLOGY_LINESTRIP_ADJ)
-		.value("TRIANGLELIST_ADJ", D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST_ADJ)
-		.value("TRIANGLESTRIP_ADJ", D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP_ADJ);
-
-	class_<ModelGenerator, std::shared_ptr<ModelGenerator>, boost::noncopyable>("ModelGenerator")
-		.def(init<VertexType, D3D_PRIMITIVE_TOPOLOGY>())
-		.def("AddVertexBatch", &ModelGenerator::AddVertexBatch)
-		.def("SetParts", &ModelGenerator::SetParts)
-		.def("Finalize", &ModelGenerator::Finalize);
-
-	class_<ModelStorageDescription>("ModelStorageDescription", init<bool, bool, bool>());
-
-	class_<ModelSlice>("ModelSlice")
-		.def(init<unsigned int, unsigned int>())
-		.add_property("start", boost::python::make_getter(&ModelSlice::start), boost::python::make_setter(&ModelSlice::start))
-		.add_property("length", boost::python::make_getter(&ModelSlice::length), boost::python::make_setter(&ModelSlice::length));
-
-	CreateMap<string, ModelSlice>("String", "ModelSlice");
-
-	class_<ResourceIdentifier, boost::noncopyable>("ResourceIdentifier")
-		.def("StripSubmodel", &ResourceIdentifier::StripSubmodel)
-		.def("GetSubmodel", &ResourceIdentifier::GetSubmodel)
-		.def("AddSubmodel", &ResourceIdentifier::AddSubmodel)
-		.def("GetNewModelName", &ResourceIdentifier::GetNewModelName)
-		.def("GetConstantModelName", &ResourceIdentifier::GetConstantModelName);
-
 	StlInterface();
 	EntitySpecificationInterface();
 	GraphicsObjectInterface(scene_registration);
@@ -176,4 +122,5 @@ BOOST_PYTHON_MODULE(scene_system_) {
 	HeadsetInterface();
 	GraphicsResourcesInterface();
 	PoseInterface();
+	ModelCreationInterface();
 }
